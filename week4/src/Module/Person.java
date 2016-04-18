9ac9885098eaf7e1696b8f0df47835262084722a
ShinSooyong <shinsy1023@gmail.com>
2016-04-18 21:32:07 +0900

    final

---
+++ week4/src/Module/Person.java
@@ -0,0 +1,19 @@
+package Module;
+
+/**
+ * Created by shinsy1023 on 2016. 4. 5..
+ */
+public class Person {
+    protected String name;
+    protected String sex;
+    protected String address;
+    protected String P_number;
+
+    public Person(String n, String s, String a, String num){
+        this.name=n;
+        this.sex=s;
+        this.address=a;
+        this.P_number=num;
+    }
+}
+
